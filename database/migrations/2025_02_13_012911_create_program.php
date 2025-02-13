0d62fde5b56f86a0e72701155762a44d49542f98
lafathalfath <iqnaraidan12@gmail.com>
2025-02-13 08:54:02 +0700

    CREATE: migrations

---
+++ database/migrations/2025_02_13_012911_create_program.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('program', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('type_id');
+            $table->string('title')->unique();
+            $table->string('image_url')->unique();
+            $table->text('description')->unique();
+            $table->timestamps();
+            
+            $table->foreign('type_id')->references('id')->on('m_program_type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('program');
+    }
+};
---
+++ database/migrations/2025_02_13_013128_create_about_page.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('about_page', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('desctiption');
+            $table->string('image_1_url');
+            $table->string('image_2_url');
+            $table->text('vision');
+            $table->text('mision');
+            $table->string('bg_color');
+            $table->string('partners_title');
+            $table->text('partners_desciption');
+            $table->boolean('is_hero_visible')->default(true);
+            $table->boolean('is_vision_visible')->default(true);
+            $table->boolean('is_members_visible')->default(true);
+            $table->boolean('is_programs_visible')->default(true);
+            $table->boolean('is_partners_visible')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('about_page');
+    }
+};
