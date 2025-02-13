93869f2d5f389f29a1b3379323c0af2ffca6bbfc
Farchan <farkun>
2025-02-13 14:57:02 +0700

    pull

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\public\aboutController;
+use App\Http\Controllers\public\homeController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -11,9 +12,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', function () {
-    return view('public.index');
-})->name('home');
+Route::get('/', [homeController::class, 'index'])->name('home');
 Route::get('/about-us', [aboutController::class, 'index'])->name('about');
 Route::get('/our-works', function () {return 'OUR WORKS';})->name('works');
 Route::get('/ideas', function () {return 'IDEAS';})->name('ideas');
