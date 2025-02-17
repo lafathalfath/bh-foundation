e7cd5eac0b2a2b001435ef7b6cb0cafb3d7cb8ee
Farchan <farkun>
2025-02-17 09:56:18 +0700

    yes

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -32,8 +32,8 @@
                     <div class="px-5 py-3 {{ request()->routeIs('manage.app_settings.view') ? "bg-[$settings->primary_color] text-white" : "hover:bg-[$settings->primary_color]/[0.5] text-gray-300 hover:text-white" }}">
                         <a href="{{ route('manage.app_settings.view') }}">App Settings</a>
                     </div>
-                    <div class="px-5 py-3 {{ request()->routeIs('admin.front_page.view') ? "bg-[$settings->primary_color] text-white" : "hover:bg-[$settings->primary_color]/[0.5] text-gray-300 hover:text-white" }}">
-                        <a href="{{ route('admin.front_page.view') }}">Front Page</a>
+                    <div class="px-5 py-3 {{ request()->routeIs('manage.front_page.view') ? "bg-[$settings->primary_color] text-white" : "hover:bg-[$settings->primary_color]/[0.5] text-gray-300 hover:text-white" }}">
+                        <a href="{{ route('manage.front_page.view') }}">Front Page</a>
                     </div>
                 </div>
                 <div>
