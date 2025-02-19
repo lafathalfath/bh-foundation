5741e01f96bd428da9f4a0e4139def8fff284961
Farchan <farkun>
2025-02-19 22:20:46 +0700

    fentch abdeas

--- app/Http/Controllers/public/IdeasController.php
+++ app/Http/Controllers/public/IdeasController.php
@@ -4,14 +4,25 @@ namespace App\Http\Controllers\public;
 
 use App\Http\Controllers\Controller;
 use App\Models\Ideas;
+use App\Models\mProgramType;
 use Illuminate\Http\Request;
 
 class IdeasController extends Controller
 {
     public function index() {
         $ideas = Ideas::first();
+        $program_type = mProgramType::where('name', 'Scholarship')->select('id')->first();
+        $scholarship = $program_type->program()->select([
+            'id',
+            'title',
+            'image_url', 
+            'description',
+            'published_at',
+            'views',
+        ])->where('published', true)->paginate(10);
         return view('public.ideas', [
-            'ideas' => $ideas
+            'ideas' => $ideas,
+            'scholarship' => $scholarship
         ]);
     }
 }
